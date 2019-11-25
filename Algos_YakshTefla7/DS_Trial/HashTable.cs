cad1cc6052d35b278464bf43171435ac41193f4b
eslam.eltaher <eslam.mohamed@linkdev.com>
2019-11-25 18:45:48 +0200

    Data Structure Trials (Hash Table)

---
+++ Algos_YakshTefla7/DS_Trial/HashTable.cs
@@ -0,0 +1,61 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Algos_YakshTefla7.DS_Trial
+//{
+//    public class HashTable <T1, T2>
+//    {
+//        LinkedList<KeyValuePair<T1, T2>>[]  data;
+
+//        public HashTable()
+//        {
+//            data = new LinkedList<KeyValuePair<T1,T2>>[int.MaxValue / 10]; 
+//        }
+//        public void Insert(T1 key, T2 value)
+//        {
+//            int itemHashCode = getHashCode(key);
+//            if (data[itemHashCode] == null)
+//                data[itemHashCode] = new LinkedList<KeyValuePair<T1, T2>>();
+
+//            data[itemHashCode].AddLast(new KeyValuePair<T1, T2>(key, value));
+//        }
+
+//        public T2 Get(T1 key)
+//        {
+//            T2 value = default(T2);
+
+//            int itemHashCode = getHashCode(key);
+
+//            if (data[itemHashCode] != null)
+//            {
+//                foreach (var item in data[itemHashCode])
+//                {
+//                    if (item.Key.Equals(key))
+//                        return value = item.Value;
+//                }
+//            }
+
+//            return value;
+//        }
+
+//        private int getHashCode(T1 item)
+//        {
+//            return Math.Abs(item.GetHashCode() / 100);
+//        }
+//    }
+
+//    public class KeyValuePair<T1, T2>
+//    {
+//        public T1 Key { get; set; }
+//        public T2 Value { get; set; }
+
+//        public KeyValuePair(T1 key, T2 value)
+//        {
+//            Key = key;
+//            Value = value;
+//        }
+//    }
+//}
---
+++ Algos_YakshTefla7/DS_Trial/ProgramHashTable.cs
@@ -0,0 +1,45 @@
+﻿//using Algos_YakshTefla7.DS_Trial;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Algos_YakshTefla7
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            // The code provided will print ‘Hello World’ to the console.
+//            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+//            Console.WriteLine("Hello World!");
+//            Console.ReadKey();
+
+//            HashTable<string, string> myHashTable = new HashTable<string, string>();
+
+//            int mil = 1000000 * 10;
+
+//            for(int i = 0; i < mil; i ++)
+//            {
+//                if (i % 1000000 == 0)
+//                    Console.WriteLine(i / 1000000);
+//                myHashTable.Insert("Key" + i.ToString(), "Value" + i.ToString());
+//            }
+
+//            myHashTable.Insert("Key2", "Value2");
+//            myHashTable.Insert("Key3", "Value3");
+
+//            for (int i = 0; i < mil; i++)
+//                Console.WriteLine(myHashTable.Get("Key" + i.ToString()));
+
+//            Console.WriteLine(myHashTable.Get("Key1"));
+//            Console.WriteLine(myHashTable.Get("Key2"));
+//            Console.WriteLine(myHashTable.Get("Key3"));
+
+//            Console.ReadLine();
+
+//            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+//        }
+//    }
+//}
