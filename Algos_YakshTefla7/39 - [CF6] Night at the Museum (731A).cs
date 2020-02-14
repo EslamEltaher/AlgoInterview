141d4f99c21ee541fe668c1ce53b75bf2bf74899
eslameltaher <eslam.eltaher@hotmail.com>
2020-02-14 12:09:47 +0200

    From Dec 8th

---
+++ Algos_YakshTefla7/39 - [CF6] Night at the Museum (731A).cs
@@ -0,0 +1,46 @@
+﻿////39 http://codeforces.com/problemset/problem/731/a
+
+//using System;
+//using System.Collections.Generic;
+////using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace algos_yakshtefla7
+//{
+//    class program
+//    {
+//        static void Main(string[] args)
+//        {
+//            string word = Console.ReadLine();
+//            Console.WriteLine(NightAtTheMuseum(word));
+//        }
+
+//        static int NightAtTheMuseum(string word)
+//        {
+//            int count = 0;
+//            //char prev = 'a';
+//            int prev = 0;
+
+//            char[] chars = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+//                'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+//            for (int i = 0; i < word.Length; i++)
+//            {
+//                int indexOfChar = 0;
+//                for (int j = 0; j < chars.Length; j++)
+//                {
+//                    if (chars[j] == word[i])
+//                    {
+//                        indexOfChar = j;
+//                        break;
+//                    }
+//                }
+//                count += Math.Min(Math.Min(Math.Abs(indexOfChar - prev), Math.Abs(chars.Length + indexOfChar - prev)), Math.Abs(chars.Length + prev - indexOfChar));
+//                prev = indexOfChar;
+//            }
+
+//            return count;
+//        }
+//    }
+//}
---
+++ Algos_YakshTefla7/40 - [CF7] Shaass and Oskols (294A).cs
@@ -0,0 +1,55 @@
+﻿////40 https://codeforces.com/problemset/problem/294/a
+
+//using System;
+//using System.Collections.Generic;
+////using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace algos_yakshtefla7
+//{
+//    class program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int n = Convert.ToInt32(Console.ReadLine());
+//            int[] wiresBefore = Array.ConvertAll(Console.ReadLine().Split(' '), el => Convert.ToInt32(el));
+//            int m = Convert.ToInt32(Console.ReadLine());
+//            int[][] events = new int[m][];
+//            for (int i = 0; i < m; i++)
+//            {
+//                events[i] = Array.ConvertAll(Console.ReadLine().Split(' '), el => Convert.ToInt32(el));
+//            }
+//            int[] wires = getWires(wiresBefore, events);        //the actual function
+//            for (int i = 0; i < n; i++)
+//            {
+//                Console.WriteLine(wires[i]);
+//            }
+//            //Console.ReadLine();
+//        }
+//        static int[] getWires(int[] w, int[][] events)
+//        {
+//            for (int i = 0; i < events.Length; i++)
+//            {
+//                int x = events[i][0] - 1;
+//                int y = events[i][1] - 1;
+
+//                //left i-1
+//                //w[x] = 0;
+//                if(x - 1 > 0)
+//                {
+//                    w[x - 1] += y;
+//                }
+                
+//                //right i+1
+//                if(x + 1 < w.Length)
+//                {
+//                    w[x + 1] += w[x] - (y + 1);
+//                }
+
+//                w[x] = 0;
+//            }
+//            return w;
+//        }
+//    }
+//}
