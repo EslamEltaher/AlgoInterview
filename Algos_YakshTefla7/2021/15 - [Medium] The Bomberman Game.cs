5dac30d648bf42eeffd2be59d4d8e33c0718765f
eslameltaher <eslam.eltaher@hotmail.com>
2021-07-27 20:07:50 +0200

    solved 16,  failed in 17

--- Algos_YakshTefla7/2021/15 - [Medium] The Bomberman Game.cs
+++ Algos_YakshTefla7/2021/15 - [Medium] The Bomberman Game.cs
@@ -1,159 +1,159 @@
-﻿//https://www.hackerrank.com/challenges/bomber-man/problem
-using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
+﻿////https://www.hackerrank.com/challenges/bomber-man/problem
+//using System.CodeDom.Compiler;
+//using System.Collections.Generic;
+//using System.Collections;
+//using System.ComponentModel;
+//using System.Diagnostics.CodeAnalysis;
+//using System.Globalization;
+//using System.IO;
+//using System.Linq;
+//using System.Reflection;
+//using System.Runtime.Serialization;
+//using System.Text.RegularExpressions;
+//using System.Text;
+//using System;
 
-namespace Algos_YakshTefla7._2021
-{
+//namespace Algos_YakshTefla7._2021
+//{
 
-    class Result
-    {
+//    class Result
+//    {
 
-        /*
-         * Complete the 'bomberMan' function below.
-         *
-         * The function is expected to return a STRING_ARRAY.
-         * The function accepts following parameters:
-         *  1. INTEGER n
-         *  2. STRING_ARRAY grid
-         */
+//        /*
+//         * Complete the 'bomberMan' function below.
+//         *
+//         * The function is expected to return a STRING_ARRAY.
+//         * The function accepts following parameters:
+//         *  1. INTEGER n
+//         *  2. STRING_ARRAY grid
+//         */
 
-        public static List<string> bomberMan(int n, List<string> grid)
-        {
-            int r = grid.Count;
-            int c = grid[0].Length;
+//        public static List<string> bomberMan(int n, List<string> grid)
+//        {
+//            int r = grid.Count;
+//            int c = grid[0].Length;
 
-            //init
-            //1. nothing
-            //2. plants all
-            //3. stops
+//            //init
+//            //1. nothing
+//            //2. plants all
+//            //3. stops
 
-            //0.     ..0..
-            //1.     ..0..
-            //2.     00000
-            //3.     0...0
-            //4.     00000
-            //5.     ..0..
-            //6.     00000
-            //7.     0...0
-            //8.     00000
-            //9.     ..0..
+//            //0.     ..0..
+//            //1.     ..0..
+//            //2.     00000
+//            //3.     0...0
+//            //4.     00000
+//            //5.     ..0..
+//            //6.     00000
+//            //7.     0...0
+//            //8.     00000
+//            //9.     ..0..
 
-            if (n == 0 || n == 1)
-                return grid;
+//            if (n == 0 || n == 1)
+//                return grid;
 
-            if (n % 2 == 0)
-            {
-                string all = "";
+//            if (n % 2 == 0)
+//            {
+//                string all = "";
 
-                for (int i = 0; i < c; i++)
-                    all += "0";
+//                for (int i = 0; i < c; i++)
+//                    all += "0";
 
-                grid = grid.Select(row => all).ToList();
-            }
+//                grid = grid.Select(row => all).ToList();
+//            }
 
-            if(true)
-            {
-                List<string> newGrid = new List<string>();
-                for(int i = 0; i < grid.Count; i++)
-                {
-                    string res = "";
-                    for (int j = 0; j < c; j++)
-                    {
-                        if(
-                            (grid[i][j] == 'O')
-                            || (i > 0 && grid[i-1][j] == 'O') 
-                            || (i < r - 1 && grid[i + 1][j] == 'O')
-                            || (j > 0 && grid[i][j - 1] == 'O')
-                            || (j < c - 1 && grid[i][j + 1] == 'O')
-                            )
-                        {
-                            res += ".";
-                        }
-                        else
-                        {
-                            res += "O";
-                        }
-                    }
+//            if(true)
+//            {
+//                List<string> newGrid = new List<string>();
+//                for(int i = 0; i < grid.Count; i++)
+//                {
+//                    string res = "";
+//                    for (int j = 0; j < c; j++)
+//                    {
+//                        if(
+//                            (grid[i][j] == 'O')
+//                            || (i > 0 && grid[i-1][j] == 'O') 
+//                            || (i < r - 1 && grid[i + 1][j] == 'O')
+//                            || (j > 0 && grid[i][j - 1] == 'O')
+//                            || (j < c - 1 && grid[i][j + 1] == 'O')
+//                            )
+//                        {
+//                            res += ".";
+//                        }
+//                        else
+//                        {
+//                            res += "O";
+//                        }
+//                    }
 
-                    newGrid.Add(res);
-                }
-                grid = newGrid;
-            }
+//                    newGrid.Add(res);
+//                }
+//                grid = newGrid;
+//            }
 
-            if(n % 4 == 1)
-            {
-                List<string> newGrid = new List<string>();
-                for (int i = 0; i < grid.Count; i++)
-                {
-                    string res = "";
-                    for (int j = 0; j < c; j++)
-                    {
-                        if (
-                            (grid[i][j] == 'O')
-                            || (i > 0 && grid[i - 1][j] == 'O')
-                            || (i < r - 1 && grid[i + 1][j] == 'O')
-                            || (j > 0 && grid[i][j - 1] == 'O')
-                            || (j < c - 1 && grid[i][j + 1] == 'O')
-                            )
-                        {
-                            res += ".";
-                        }
-                        else
-                        {
-                            res += "O";
-                        }
-                    }
+//            if(n % 4 == 1)
+//            {
+//                List<string> newGrid = new List<string>();
+//                for (int i = 0; i < grid.Count; i++)
+//                {
+//                    string res = "";
+//                    for (int j = 0; j < c; j++)
+//                    {
+//                        if (
+//                            (grid[i][j] == 'O')
+//                            || (i > 0 && grid[i - 1][j] == 'O')
+//                            || (i < r - 1 && grid[i + 1][j] == 'O')
+//                            || (j > 0 && grid[i][j - 1] == 'O')
+//                            || (j < c - 1 && grid[i][j + 1] == 'O')
+//                            )
+//                        {
+//                            res += ".";
+//                        }
+//                        else
+//                        {
+//                            res += "O";
+//                        }
+//                    }
 
-                    newGrid.Add(res);
-                }
-                grid = newGrid;
-            }
+//                    newGrid.Add(res);
+//                }
+//                grid = newGrid;
+//            }
 
-            return grid;
-        }
+//            return grid;
+//        }
 
-    }
+//    }
 
-    class Solution
-    {
-        public static void Main(string[] args)
-        {
-            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+//    class Solution
+//    {
+//        public static void Main(string[] args)
+//        {
+//            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
-            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+//            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
 
-            int r = Convert.ToInt32(firstMultipleInput[0]);
+//            int r = Convert.ToInt32(firstMultipleInput[0]);
 
-            int c = Convert.ToInt32(firstMultipleInput[1]);
+//            int c = Convert.ToInt32(firstMultipleInput[1]);
 
-            int n = Convert.ToInt32(firstMultipleInput[2]);
+//            int n = Convert.ToInt32(firstMultipleInput[2]);
 
-            List<string> grid = new List<string>();
+//            List<string> grid = new List<string>();
 
-            for (int i = 0; i < r; i++)
-            {
-                string gridItem = Console.ReadLine();
-                grid.Add(gridItem);
-            }
+//            for (int i = 0; i < r; i++)
+//            {
+//                string gridItem = Console.ReadLine();
+//                grid.Add(gridItem);
+//            }
 
-            List<string> result = Result.bomberMan(n, grid);
+//            List<string> result = Result.bomberMan(n, grid);
 
-            textWriter.WriteLine(String.Join("\n", result));
+//            textWriter.WriteLine(String.Join("\n", result));
 
-            textWriter.Flush();
-            textWriter.Close();
-        }
-    }
+//            textWriter.Flush();
+//            textWriter.Close();
+//        }
+//    }
 
-}
+//}
---
+++ Algos_YakshTefla7/2021/16 - [Medium] Almost Sorted - 50pts.cs
@@ -0,0 +1,161 @@
+﻿////https://www.hackerrank.com/challenges/almost-sorted/problem    
+
+//using System.CodeDom.Compiler;
+//using System.Collections.Generic;
+//using System.Collections;
+//using System.ComponentModel;
+//using System.Diagnostics.CodeAnalysis;
+//using System.Globalization;
+//using System.IO;
+//using System.Linq;
+//using System.Reflection;
+//using System.Runtime.Serialization;
+//using System.Text.RegularExpressions;
+//using System.Text;
+//using System;
+
+
+//namespace Algos_YakshTefla7._2021
+//{
+
+//    class Result
+//    {
+
+//        /*
+//         * Complete the 'almostSorted' function below.
+//         *
+//         * The function accepts INTEGER_ARRAY arr as parameter.
+//         */
+
+//        public static void almostSorted(List<int> arr)
+//        {
+//            int swapL = -1, swapR = -1;
+//            int revStart = -1, revEnd = -1;
+
+//            for (int i = 1; i < arr.Count; i++)
+//            {
+//                ////////////// Reverse
+//                if (arr[i] < arr[i - 1])
+//                {
+//                    if (revStart == -1)
+//                    {
+//                        revStart = i - 1;
+//                    }
+//                    else if (revEnd != -1)
+//                    {
+//                        revEnd = 0; // to signal that it needs more than one reverse
+//                    }
+//                }
+//                else if (arr[i] > arr[i - 1])
+//                {
+//                    if (revStart != -1 && revEnd == -1)
+//                    {
+//                        revEnd = i - 1;
+//                    }
+//                }
+
+
+//                ////SWAP
+//                if (arr[i] < arr[i - 1])
+//                {
+//                    if (swapL == -1)
+//                    {
+//                        swapL = i;
+//                    }
+//                    else
+//                    {
+//                        if (swapR == -1)
+//                        {
+//                            swapR = i;
+
+//                            //edge case (if swapR is smaller than swapL)
+//                            if (arr[i] < arr[swapL])
+//                            {
+//                                swapL--;
+//                            }
+//                        }
+//                        else
+//                        {
+//                            swapL = -2; //more than one swap
+//                        }
+
+
+//                        if (swapL == i - 1)
+//                        {
+//                            swapL--;
+//                        }
+//                    }
+//                }
+//            }
+
+//            //reverse if end not determined
+//            if (revStart != -1 && revEnd == -1)
+//            {
+//                if (arr[arr.Count - 1] < arr[revStart])
+//                    revEnd = arr.Count - 1;
+//                else
+//                {
+//                    revEnd = revStart;
+//                    revStart--;
+//                }
+//            }
+
+//            //swap if Right not determined
+//            if (swapL != -1 && swapR == -1)
+//            {
+//                swapR = swapL;
+//                swapL = swapL - 1;
+//            }
+
+//            if (swapL > -1)
+//            {
+//                if (
+//                    (arr[swapR] <= arr[swapL + 1])
+//                    && (arr[swapL] >= arr[swapR - 1])
+//                    && (swapL <= 0 || arr[swapR] > arr[swapL - 1])
+//                    && ((swapR >= arr.Count - 1) || arr[swapL] < arr[swapR + 1])
+//                   )
+//                {
+//                    Console.WriteLine("yes");
+//                    Console.WriteLine("swap " + (swapL + 1) + " " + (swapR + 1));
+//                    return;
+//                }
+//            }
+
+//            if (revStart != -1 && revEnd != 0)
+//            {
+//                if (
+//                    (revStart <= 0 || arr[revEnd] > arr[revStart - 1])
+//                    && (revEnd >= arr.Count - 1 || arr[revStart] < arr[revEnd + 1])
+//                    )
+//                {
+//                    Console.WriteLine("yes\nreverse " + (revStart + 1) + " " + (revEnd + 1));
+//                    return;
+//                }
+//            }
+
+//            if (revStart == -1 && revEnd == -1 && swapL == -1 && swapR == -1)
+//            {
+//                Console.WriteLine("yes");
+//                return;
+
+//            }
+
+//            Console.WriteLine("no");
+//        }
+
+//    }
+
+//    class Solution
+//    {
+//        public static void Main(string[] args)
+//        {
+//            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+//            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+//            Result.almostSorted(arr);
+//        }
+//    }
+
+//}
