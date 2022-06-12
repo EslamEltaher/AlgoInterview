2e24c5c9e589c8228c0948f04dc9d3d4762eef70
ema.eltaher <ema.eltaher@intergraph.com.sa>
2022-06-12 21:33:33 +0300

    Leet - Algorithm II Study plan - Day II

---
+++ Algos_YakshTefla7/2022/leet_algo-study-plan-II/04 - 0153 - Find Minimum in Rotated Sorted Array.cs
@@ -0,0 +1,102 @@
+﻿////https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+
+//public class Solution
+//{
+//    public int FindMin(int[] nums)
+//    {
+//        #region Working Accepted Solution
+//        //int turningPoint = -1;
+
+//        //int left = 0;
+//        //int right = nums.Length - 1;
+
+//        //int midIndex = (left + right) / 2;
+//        //int midEl = nums[midIndex];
+
+//        ////searching left
+//        //left = 0;
+//        //right = midIndex - 1;
+//        //while (left <= right)
+//        //{
+//        //    int mid = (left + right) / 2;
+
+//        //    //found element in the left sequence
+//        //    //we go right to find the last element in the left sequence
+//        //    if (nums[mid] > midEl)
+//        //    {
+//        //        turningPoint = mid;
+
+//        //        left = mid + 1;
+//        //    }
+
+//        //    //didn't find an element from the left sequence going left
+//        //    else
+//        //    {
+//        //        right = mid - 1;
+//        //    }
+//        //}
+
+
+//        ////if turning point not found this means that the left sequence is more than half of the array
+//        ////going right to find the beginning of the right sequence
+
+//        //left = midIndex + 1;
+//        //right = nums.Length - 1;
+//        //if (turningPoint == -1)
+//        //{
+//        //    while (left <= right)
+//        //    {
+//        //        int mid = (left + right) / 2;
+
+//        //        //if found we go left to find the first element of the right sequence
+//        //        if (nums[mid] < midEl)
+//        //        {
+//        //            turningPoint = mid - 1;
+
+//        //            right = mid - 1;
+//        //        }
+
+//        //        //not found we go further right
+//        //        else
+//        //        {
+//        //            left = mid + 1;
+//        //        }
+//        //    }
+//        //}
+
+//        //return nums[turningPoint + 1];
+//        #endregion
+
+//        #region Trying slightly faster solution
+//        int turningPoint = -1;
+
+//        //finding the turningPoint at the ending of left sequence
+//        //the last element that is higher than Array's element
+//        int last = nums[nums.Length - 1];
+
+//        int left = 0;
+//        int right = nums.Length - 1;
+
+//        while(left <= right)
+//        {
+//            int mid = (left + right) / 2;
+
+//            //if we found an element higher than last element (left sequence)
+//            //to find the last element in the left sequence we go right
+//            if(nums[mid] > last)
+//            {
+//                turningPoint = mid;
+
+//                left = mid + 1;
+//            }
+
+//            else
+//            {
+//                right = mid - 1;
+//            }
+//        }
+
+//        return nums[turningPoint + 1];
+//        #endregion
+//    }
+//}
---
+++ Algos_YakshTefla7/2022/leet_algo-study-plan-II/05 - 0162 - Find Peak Element.cs
@@ -0,0 +1,44 @@
+﻿////https://leetcode.com/problems/find-peak-element/
+
+//public class Solution
+//{
+//    public int FindPeakElement(int[] nums)
+//    {
+//        if (nums.Length == 1 || nums[0] > nums[1])
+//            return 0;
+
+//        if (nums[nums.Length - 1] > nums[nums.Length - 2])
+//            return nums.Length - 1;
+
+
+//        #region My Linear O(N) Solution
+//        //for (int i = 1; i < nums.Length - 1; i++)
+//        //{
+//        //    if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1])
+//        //        return i;
+//        //}
+
+//        //return -1;
+//        #endregion
+
+//        #region Binary Solution from Leetcode O(LogN)
+//        int left = 1;
+//        int right = nums.Length - 2;
+
+//        while(left <= right)
+//        {
+//            int mid = (left + right) / 2;
+
+//            if (nums[mid] > nums[mid + 1] && nums[mid] > nums[mid - 1])
+//                return mid;
+
+//            else if (nums[mid] > nums[mid + 1])
+//                right = mid - 1;
+//            else
+//                left = mid + 1;
+//        }
+
+//        return -1;
+//        #endregion
+//    }
+//}
