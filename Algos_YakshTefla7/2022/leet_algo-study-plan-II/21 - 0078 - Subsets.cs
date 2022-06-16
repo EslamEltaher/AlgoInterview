c4fd60ddf534efdb135a9e056b7f16f5e4a8db4c
eslameltaher <eslam.eltaher@hotmail.com>
2022-06-16 15:29:47 +0300

    Leet - Algorithm II Study plan - Day IX

---
+++ Algos_YakshTefla7/2022/leet_algo-study-plan-II/21 - 0078 - Subsets.cs
@@ -0,0 +1,61 @@
+﻿////https://leetcode.com/problems/subsets/
+
+//using System.Collections.Generic;
+
+//public class Solution
+//{
+//    public IList<IList<int>> Subsets(int[] nums)
+//    {
+//        return Subsets(nums, 0, nums.Length - 1);
+//    }
+
+//    public IList<IList<int>> Subsets(int[] nums, int left, int right)
+//    {
+//        //if (left > nums.Length - 1)
+//        //    return new List<IList<int>>() { new List<int>() };
+
+//        //var result = new List<IList<int>>();
+
+//        //if(left == right)
+//        //{
+//        //    result.Add(new List<int>() { nums[left] });
+//        //    return result;
+//        //}
+
+//        //var list = new List<int>();
+
+//        //int i = left;
+//        //int j = right;
+
+//        //while (i <= j)
+//        //{
+//        //    list.Add(nums[i++]);
+//        //}
+//        //result.Add(list);
+
+//        //result.AddRange(Subsets(nums, left, right - 1));
+//        //result.AddRange(Subsets(nums, left + 1, right));
+
+//        ////while(right > left)
+//        ////    result.AddRange(Subsets(nums, left, --right));
+//        ///
+
+//        if(left == right)
+//        {
+//            return new List<IList<int>>() { new List<int>(), new List<int>() { nums[left] } };
+//        }
+//        var ans = new List<IList<int>>();
+//        var results = Subsets(nums, left + 1, right);
+
+//        foreach (var res in results)
+//        {
+//            var newResult = new List<int>() { nums[left] };
+//            newResult.AddRange(res);
+
+//            ans.Add(res);
+//            ans.Add(newResult);
+//        }
+
+//        return ans;
+//    }
+//}
---
+++ Algos_YakshTefla7/2022/leet_algo-study-plan-II/22 - 0090 - Subsets II.cs
@@ -0,0 +1,79 @@
+﻿////https://leetcode.com/problems/subsets-ii/
+
+//using System;
+//using System.Collections.Generic;
+
+//public class Solution
+//{
+//    public IList<IList<int>> SubsetsWithDup(int[] nums)
+//    {
+//        Array.Sort(nums);
+//        return SubsetsII(nums, 0, nums.Length - 1);
+//    }
+
+//    public IList<IList<int>> SubsetsII(int[] nums, int left, int right)
+//    {
+//        if(left > right)
+//            return new List<IList<int>>() { new List<int>() };
+
+//        if(left == right)
+//        {
+//            return new List<IList<int>>() { new List<int>(), new List<int>() { nums[left] } };
+//        }
+
+//        var result = new List<IList<int>>();
+
+//        //get the count of repeated Items
+//        int l = left;
+//        while (l < nums.Length - 1 && nums[l] == nums[l + 1])
+//        {
+//            l++;
+//        }
+//        int countL = l - left + 1;
+
+//        //make left combination from repeated elements
+//        List<IList<int>> combL = new List<IList<int>>() { new List<int>() };
+
+//        for(int i = 0; i < countL; i++)
+//        {
+//            var leftI = new List<int>();
+
+//            for(int j = 0; j < i + 1; j++)
+//            {
+//                leftI.Add(nums[left]);
+//            }
+
+//            combL.Add(leftI);
+//        }
+
+//        left = l;
+
+//        var combR = SubsetsII(nums, left + 1, right);
+
+//        var comb = combine(combL, combR);
+
+//        result.AddRange(comb);
+
+//        return result;
+//    }
+
+//    public IList<IList<int>> combine(IList<IList<int>> l1, IList<IList<int>> l2)
+//    {
+//        List<IList<int>> result = new List<IList<int>>();
+
+//        foreach (var i1 in l1)
+//        {
+//            foreach (var i2 in l2)
+//            {
+//                var newList = new List<int>();
+
+//                newList.AddRange(i1);
+//                newList.AddRange(i2);
+
+//                result.Add(newList);
+//            }
+//        }
+
+//        return result;
+//    }
+//}
